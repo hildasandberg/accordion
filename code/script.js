1e56efba417a705c0cb4a1f2cab3f32e521e697c
Hilda <hilda.sandberg@gmail.com>
2017-11-16 14:06:40 +0100

    completed

--- code/script.js
+++ code/script.js
@@ -5,3 +5,19 @@ function toggle() {
 
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggle
+
+// A function that adds and remove the class "active" on the section you click on.
+function toggle() {
+  this.classList.toggle("active")
+}
+
+// Selects and HTML element, and calls a function which will be executed when the element is clicked.
+document.getElementById("section2").onclick = toggle
+
+// A function that adds and remove the class "active" on the section you click on.
+function toggle() {
+  this.classList.toggle("active")
+}
+
+// Selects and HTML element, and calls a function which will be executed when the element is clicked.
+document.getElementById("section3").onclick = toggle
